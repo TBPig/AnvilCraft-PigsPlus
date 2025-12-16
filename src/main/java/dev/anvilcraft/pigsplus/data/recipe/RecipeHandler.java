b12b014c5fce8d707d5830d20ed94c5dfb128c1c
TBPig <147127248+TBPig@users.noreply.github.com>
2025-12-16 22:35:49 +0800

    添加末影部件合成方式

--- src/main/java/dev/anvilcraft/pigsplus/data/recipe/RecipeHandler.java
+++ src/main/java/dev/anvilcraft/pigsplus/data/recipe/RecipeHandler.java
@@ -2,10 +2,20 @@ package dev.anvilcraft.pigsplus.data.recipe;
 
 import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import dev.anvilcraft.pigsplus.init.AddonBlocks;
+import dev.anvilcraft.pigsplus.init.AddonItems;
+import dev.dubhe.anvilcraft.init.item.ModItemTags;
+import dev.dubhe.anvilcraft.init.item.ModItems;
 import dev.dubhe.anvilcraft.recipe.anvil.wrap.ItemInjectRecipe;
+import dev.dubhe.anvilcraft.recipe.anvil.wrap.TimeWarpRecipe;
 import dev.dubhe.anvilcraft.recipe.mineral.MineralFountainRecipe;
+import mezz.jei.api.constants.Tags;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
+import net.neoforged.neoforge.common.crafting.BlockTagIngredient;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
 
 public class RecipeHandler {
     public static void init(RegistrateRecipeProvider provider) {
@@ -26,5 +36,11 @@ public class RecipeHandler {
             .resultBlock(AddonBlocks.BUDDING_ECHO_SHARD)
             .save(provider);
 
+        TimeWarpRecipe.builder()
+            .requires(AddonItems.KARAKURI_COMPONENT)
+            .requires(Items.ENDER_PEARL)
+            .requires(Items.END_STONE,3)
+            .result(AddonItems.ENDER_COMPONENT,0.2f)
+            .save(provider);
     }
 }
